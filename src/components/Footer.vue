89a7af3fff61687b5a36e9487880b6d0b8df86a6
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-10 15:26:21 +0200

    [ add ] Footer structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,10 +5,56 @@ export default {
 </script>
 
 <template>
-  <h2>footer</h2>
+  <footer>
+    <div class="container">
+      <div class="foot-box d-flex">
+
+        <div class="information-footer">
+          <a href="#"><h4>Booleando s.r.l</h4></a>
+          <a href="#"><span>Informazioni legali</span></a>
+          <a href="#"><span>Informativa sulla privacy</span></a>
+          <a href="#"><span>Diritto di recesso</span></a>
+        </div>
+
+        <nav class="footer-icons">
+          <h5>Trovaci anche su</h5>
+          <ul>
+            <li><a href="#"><i class="fa-brands fa-square-twitter"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-square-pinterest"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-square-youtube"></i></a></li>
+          </ul>
+        </nav>
+
+        
+      </div>
+    </div>
+  </footer>
 </template>
 
 
-<style>
- 
+<style lang="scss" scoped>
+@use '../scss/partials/variables' as *;
+  footer {
+    background-color: black;
+    color: white;
+  }
+
+  .foot-box {
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+  }
+
+  .footer-icons h5 {
+    text-align: center;
+    padding-bottom: 5px;
+    font-size: .9rem;
+  }
+
+  .information-footer a span {
+    font-size: .8rem;
+    padding: 5px 5px 0 0;
+  }
 </style>

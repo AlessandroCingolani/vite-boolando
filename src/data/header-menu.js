df0b119543f39279fccda55c754b0eb8d73787f0
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-10 15:58:21 +0200

    [ add ] data and header-menu array of objects

---
+++ src/data/header-menu.js
@@ -0,0 +1,35 @@
+const gender = [
+  {
+    href:'#',
+    text:'Uomo'
+  },
+
+  {
+    href:'#',
+    text:'Donna'
+  },
+
+  {
+    href:'#',
+    text:'Bambino'
+  }
+]
+
+const icons = [
+  {
+    href:'#',
+    icon:'fa-regular fa-user'
+  },
+
+  {
+    href:'#',
+    icon:'fa-regular fa-heart'
+  },
+
+  {
+    href:'#',
+    icon:'fa-solid fa-bag-shopping'
+  }
+]
+
+export { gender,icons }

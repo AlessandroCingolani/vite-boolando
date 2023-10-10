5bdf8ac3d5c43a8fe0eb6173b6b0de5b82c707cd
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-10 15:23:35 +0200

    [ del ] comment

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,6 @@
 <template>
     
   <header>
-    <!-- need border trasparence -->
     <div class="top-fixed">
       <div class="container header-bar d-flex">
 
@@ -37,15 +36,15 @@
 
 
 <style lang="scss" scoped>
-@use '../scss/partials/variables';
+@use '../scss/partials/variables' as *;
   header .top-fixed {
     width: 100%;
     position: fixed;
-    box-shadow: 0px 0px 15px gray;
+    box-shadow: 0px 0px 15px $brand-color;
     top:0;
     left: 0;
     padding: 12px;
-    background-color:rgb(255, 105, 0);
+    background-color:$header-background;
     z-index: 999;
 
   }

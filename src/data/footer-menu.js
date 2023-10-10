808dbeb1fe6ec07a1cee73f9f5b16a22616d402d
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-10 16:16:56 +0200

    [ add ] data footer

---
+++ src/data/footer-menu.js
@@ -0,0 +1,48 @@
+const info = [
+  {
+    href:'#',
+    text:'Informazioni legali'
+  },
+
+  {
+    href:'#',
+    text:'Informativa sulla privacy'
+  },
+
+  {
+    href:'#',
+    text:'Diritto di recesso'
+  }
+]
+
+
+
+const social = [
+  {
+    href:'#',
+    icon:'fa-brands fa-square-twitter'
+  },
+
+  {
+    href:'#',
+    icon:'fa-brands fa-square-facebook'
+  },
+
+  {
+    href:'#',
+    icon:'fa-brands fa-square-instagram'
+  },
+
+  {
+    href:'#',
+    icon:'fa-brands fa-square-pinterest'
+  },
+
+  {
+    href:'#',
+    icon:'fa-brands fa-square-youtube'
+  }
+]
+
+
+export { info,social }
